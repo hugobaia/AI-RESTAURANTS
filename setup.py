f403d7c8424ac0abfd04fb829a6d639a268c3301
hugorbs <hrbs2@cin.ufpe.br>
2018-06-03 18:31:12 -0300

    Update setup file

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='recommendus',
       author_email='hugotpa@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       setup_requires=['numpy'],
-      install_requires=['Flask', 'geopy', 'pandas', 'numpy'],
+      install_requires=['Flask', 'numpy'],
       packages=find_packages(),
       )
