a1fe679b0fd89b17f82bdf1c4dafeaf36891d1eb
hugorbs <hrbs2@cin.ufpe.br>
2018-06-03 17:25:21 -0300

    Update setup file

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='recommendus',
       author='Hugo Silva',
       author_email='hugotpa@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask'],
       packages=find_packages(),
       )
