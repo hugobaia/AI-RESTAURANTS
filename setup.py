ed7a562e3f91110ba74733bbd120df604b3e1016
hugorbs <hrbs2@cin.ufpe.br>
2018-06-03 17:22:25 -0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='recommendus',
+      version='1.0',
+      description='Recommendus AI',
+      author='Hugo Silva',
+      author_email='hugotpa@gmail.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      packages=find_packages(),
+      )
