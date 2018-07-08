d0d67da3f07d9ad01edc9c41a0d11475f721f28a
hugorbs <hrbs2@cin.ufpe.br>
2018-07-08 15:17:56 -0300

    Change restaurants csv

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def getRecommendations():
     users = dataDict["users"]
     price = dataDict["price"]
 
-    return Recommender.getRestaurants(tags, users, price)
+    return getRestaurants(tags, users, price)
  
 if __name__ == "__main__":
     app.run()
